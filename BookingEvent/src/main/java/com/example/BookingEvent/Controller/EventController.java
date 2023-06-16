f7aee664be2514771b7bfd493636b79c773d8f36
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-16 13:09:07 +0400

    Adding EventController Class

---
+++ BookingEvent/src/main/java/com/example/BookingEvent/Controller/EventController.java
@@ -0,0 +1,5 @@
+package com.example.BookingEvent.Controller;
+
+public class EventController {
+
+}
---
+++ BookingEvent/src/test/java/com/example/BookingEvent/BookingEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
