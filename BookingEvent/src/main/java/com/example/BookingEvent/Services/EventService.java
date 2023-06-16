872ebe2a94d3072b88720b50c0a8e56a63593e55
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-16 15:07:33 +0400

    Update on class EventService, List of All Events.

--- BookingEvent/src/main/java/com/example/BookingEvent/Services/EventService.java
+++ BookingEvent/src/main/java/com/example/BookingEvent/Services/EventService.java
@@ -1,4 +1,22 @@
 package com.example.BookingEvent.Services;
 
+
+import com.example.BookingEvent.Models.EventModel;
+import com.example.BookingEvent.Repository.EventRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public class EventService {
+    @Autowired
+    EventRepo   eventRepo;
+
+
+    List<EventModel> ListOfAllEvents(){
+        return eventRepo.findAll();
+
+    }
 }
