b8227e2b8f998159c72cbd65f78516663e93623a
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-16 15:10:13 +0400

    Update on class UserService, List of All User.

--- BookingEvent/src/main/java/com/example/BookingEvent/Services/UserService.java
+++ BookingEvent/src/main/java/com/example/BookingEvent/Services/UserService.java
@@ -1,4 +1,20 @@
 package com.example.BookingEvent.Services;
 
+
+import com.example.BookingEvent.Models.UserModel;
+import com.example.BookingEvent.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepo  userRepo;
+
+    List<UserModel> ListOfAllUsers(){
+        return userRepo.findAll();
+    }
 }
