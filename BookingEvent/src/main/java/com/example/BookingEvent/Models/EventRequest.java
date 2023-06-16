89e1925cef910369d628a13507a2da0a8b780e6f
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-16 14:27:13 +0400

    Adding the variable> Date, Location, AvailableTickets.

--- BookingEvent/src/main/java/com/example/BookingEvent/Models/EventRequest.java
+++ BookingEvent/src/main/java/com/example/BookingEvent/Models/EventRequest.java
@@ -4,9 +4,15 @@ package com.example.BookingEvent.Models;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 
 public class EventRequest {
     private String eventName;
+    private LocalDate eventDate;
+    private String  eventLocation;
+    private int NumberOfAvailableTicket;
+
 }
