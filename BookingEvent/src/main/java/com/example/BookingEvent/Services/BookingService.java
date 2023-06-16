f0ef7f4dd6286e2881a162c0cdb33435ddfd521b
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-16 15:05:32 +0400

    Update on Class BookingService.

--- BookingEvent/src/main/java/com/example/BookingEvent/Services/BookingService.java
+++ BookingEvent/src/main/java/com/example/BookingEvent/Services/BookingService.java
@@ -1,4 +1,21 @@
 package com.example.BookingEvent.Services;
 
+
+import com.example.BookingEvent.Models.BookingModel;
+import com.example.BookingEvent.Repository.BookingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public class BookingService {
+@Autowired
+    BookingRepo bookingRepo;
+
+    List<BookingModel> listOfAllBooking(){
+        return bookingRepo.findAll();
+
+    }
 }
